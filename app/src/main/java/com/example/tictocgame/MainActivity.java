cc0c03cfec1973063dd72b75ef3719877a009e86
Mithun Kumar Sharma <mithun@Mithuns-MacBook-Pro.local>
2020-12-24 19:28:24 +0000

    Refactoring commit

--- app/src/main/java/com/example/tictocgame/MainActivity.java
+++ app/src/main/java/com/example/tictocgame/MainActivity.java
@@ -5,105 +5,90 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer mediaPlayer;
-    int activePlayer = 0;
-    int[] gameState = {2,2,2,2,2,2,2,2,2};
-    int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
-    boolean gameActive = true;
+    MediaPlayer mMediaPlayer;
+    int mActivePlayer = 0;
+    int[] mGameState = {2,2,2,2,2,2,2,2,2};
+    int[][] mWinningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    boolean mGameActive = true;
+
+    TextView mWinnerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWinnerTextView = (TextView) findViewById(R.id.winnerTextView);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if(mediaPlayer == null){
-            mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        if(mMediaPlayer == null){
+            mMediaPlayer = MediaPlayer.create(this, R.raw.music);
         }
-
-        mediaPlayer.start();
+        mMediaPlayer.start();
     }
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
-        if (gameState[tappedCounter] == 2 && gameActive) {
+        if (mGameState[tappedCounter] == 2 && mGameActive) {
             counter.setTranslationY(-1500);
-            gameState[tappedCounter] = activePlayer;
-            if (activePlayer == 1) {
+            mGameState[tappedCounter] = mActivePlayer;
+            if (mActivePlayer == 1) {
                 counter.setImageResource(R.drawable.yellow);
-                activePlayer = 0;
+                mActivePlayer = 0;
             } else {
                 counter.setImageResource(R.drawable.red);
-                activePlayer = 1;
+                mActivePlayer = 1;
             }
 
             counter.animate().translationYBy(1500).rotation(3600).setDuration(100);
 
-            for (int[] winningPosition : winningPositions) {
-                TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+            for (int[] winningPosition : mWinningPositions) {
                 String winner = "";
-                if (gameState[winningPosition[0]] == gameState[winningPosition[1]]
-                        && gameState[winningPosition[1]] == gameState[winningPosition[2]]
-                        && gameState[winningPosition[0]] != 2) {
+                if (mGameState[winningPosition[0]] == mGameState[winningPosition[1]]
+                        && mGameState[winningPosition[1]] == mGameState[winningPosition[2]]
+                        && mGameState[winningPosition[0]] != 2) {
 
-                    gameActive = false;
-                    if (activePlayer == 0) {
+                    mGameActive = false;
+                    if (mActivePlayer == 0) {
                         winner = "Yellow ";
                     } else  {
                         winner = "Red ";
                     }
+                    mWinnerTextView.setText(winner + " has won!");
 
-                    winnerTextView.setText(winner + " has won!");
-
-                    winnerTextView.setVisibility(View.VISIBLE);
-
+                    mWinnerTextView.setVisibility(View.VISIBLE);
                 }
             }
         }
     }
 
     public void playAgain(View view) {
-
-        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
-
-        winnerTextView.setVisibility(View.INVISIBLE);
-
+        mWinnerTextView.setVisibility(View.INVISIBLE);
         GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
-
         for(int i=0; i<gridLayout.getChildCount(); i++) {
-
             ImageView counter = (ImageView) gridLayout.getChildAt(i);
-
             counter.setImageDrawable(null);
-
         }
 
-        for (int i=0; i<gameState.length; i++) {
-
-            gameState[i] = 2;
-
+        for (int i = 0; i< mGameState.length; i++) {
+            mGameState[i] = 2;
         }
-
-        activePlayer = 0;
-
-        gameActive = true;
-
+        mActivePlayer = 0;
+        mGameActive = true;
     }
 
     public void exitGame(View view){
-        mediaPlayer.stop();
+        mMediaPlayer.stop();
         finish();
     }
 }
